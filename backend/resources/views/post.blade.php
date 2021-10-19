ad9da9b19891b31edb9cbf4c1fbac03ad2d512ac
riku929hr <sugarhorn0929@gmail.com>
2021-10-19 16:32:25 +0900

    [Feat] Blog Template Page (Finish Chap.2-9)

---
+++ backend/resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" href="../app.css">
+<title>My Blog</title>
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+    <a href="/"></a>
+</body>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
+
+Route::get('/posts/{post}', function($slug){
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+
+    if(!file_exists($path)){
+        abort(404);
+    }
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+');
