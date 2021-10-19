781e7c854145d618e1fb23cd8dc38f13b093226c
riku929hr <sugarhorn0929@gmail.com>
2021-10-20 00:51:50 +0900

    [feat] Sort by Date (Finish Chap. 2.13)

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -35,7 +35,8 @@ class Post
                     $document->body(),
                     $document->slug
                 )
-            );
+            )
+            ->sortByDesc('date');
     }
 
     public static function find($slug)
