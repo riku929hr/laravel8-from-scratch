2f79b0a0b1028c87948563eaf48a8fbd3907341c
riku929hr <sugarhorn0929@gmail.com>
2021-10-20 01:05:48 +0900

    [fix] change php code to blade notation

--- backend/resources/views/posts.blade.php
+++ backend/resources/views/posts.blade.php
@@ -5,13 +5,16 @@
 
 <body>
 
-    <?php foreach ($posts as $post): ?>
+    @foreach ($posts as $post)
     <article>
-        <h1><a href="/posts/<?= $post->slug; ?>"><?= $post->title; ?></a></h1>
-
+        <h1>
+            <a href="/posts/{{ $post->slug; }}">
+                {{ $post->title }}
+            </a>
+        </h1>
         <div>
-            <?= $post->excerpt; ?>
+            {{ $post->excerpt; }}
         </div>
     </article>
-    <?php endforeach; ?>
+    @endforeach
 </body>
