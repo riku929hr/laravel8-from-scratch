06d7a98a6f1fef81113dd7726d9416799194022d
riku929hr <sugarhorn0929@gmail.com>
2021-10-28 17:28:37 +0900

    [feat] Added alpinejs and catefory dropdown

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -17,7 +17,8 @@ use App\Models\Post, App\Models\User;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -29,12 +30,14 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
