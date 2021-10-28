ddcf3827105a20329d2e4175ee1634a2e1fafc28
riku929hr <sugarhorn0929@gmail.com>
2021-10-28 21:46:10 +0900

    [fix] removed unused object

--- backend/resources/views/components/dropdown.blade.php
+++ backend/resources/views/components/dropdown.blade.php
@@ -8,7 +8,7 @@
 
 
     {{-- Link --}}
-    <div x-show="show" class="py-2 absolute w-full bg-gray-100 mt-2 rounded-xl z-50" style="display: none">
+    <div x-show="show" class="py-2 absolute w-full bg-gray-100 mt-2 rounded-xl z-50 overflow-auto max-h-52" style="display: none">
         {{ $slot }}
     </div>
 
