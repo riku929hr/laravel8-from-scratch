cf28d9f2dc966fc65376919dc037d33b206a24c5
riku929hr <sugarhorn0929@gmail.com>
2021-10-29 20:19:30 +0900

    [add] category dropdown view class

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -13,8 +13,6 @@ class PostController extends Controller
     {
         return view('posts', [
             'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-            'categories' => Category::all(),
-            'currentCategory' => Category::firstWhere('slug', request('category'))
         ]);
     }
 
