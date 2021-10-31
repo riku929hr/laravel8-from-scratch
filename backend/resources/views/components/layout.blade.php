488a270d7d1494bf8962417d20738a7c54274190
riku929hr <sugarhorn0929@gmail.com>
2021-11-01 01:41:30 +0900

    [feat] hide message after 3min

--- backend/resources/views/components/layout.blade.php
+++ backend/resources/views/components/layout.blade.php
@@ -57,7 +57,11 @@
         </footer>
     </section>
     @if (session()->has('success'))
-    <div class="bg-blue-500 bottom-3 fixed px-4 py-2 right-3 rounded-xl text-sm text-white">
+    <div x-data="{ show: true }"
+         x-init="setTimeout(() => show = false, 4000)"
+         x-show="show"
+         class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm"
+    >
         <p>{{ session('success') }}</p>
     </div>
     @endif
