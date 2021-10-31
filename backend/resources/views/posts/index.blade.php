c47a033ccce703f9e6fd90937fa7cbabaaece53c
riku929hr <sugarhorn0929@gmail.com>
2021-10-31 14:32:29 +0900

    [feat] add pagination and fix the links

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -8,29 +8,11 @@
 
             <x-post-grid :posts="$posts"/>
 
+            {{ $posts->links() }}
+
         @else
             <p class="text-center">No Posts Yet. Please Check Back Later.</p>
         @endif
 
     </main>
-
-
-    {{-- @foreach ($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug; }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        <p>
-           Written by <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
-            <a href="/categories/{{$post->category->slug}}">
-                {{ $post->category->name }}
-            </a>
-        </p>
-        <div>
-            {{ $post->excerpt; }}
-        </div>
-    </article>
-    @endforeach --}}
 </x-layout>
