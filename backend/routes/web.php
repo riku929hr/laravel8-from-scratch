fb71e9947e25cb73b78182a93ccfe9554f8cb2fd
riku929hr <sugarhorn0929@gmail.com>
2021-11-01 21:13:10 +0900

    [feat] Login form (fin section9)

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -10,4 +10,8 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
-Route::get('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
