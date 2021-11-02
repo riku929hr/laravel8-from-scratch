30b73c8e3708e02fbc4a170708faf226015caea2
riku929hr <sugarhorn0929@gmail.com>
2021-11-03 00:46:05 +0900

    [feat] Comments section (fin chap54)

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -46,6 +46,10 @@ class Post extends Model
             )
         );
     }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 
     public function category()
     {
