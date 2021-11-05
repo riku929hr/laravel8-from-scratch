71f6653ead7e501f0f4f1e972d09377f355726e9
riku929hr <sugarhorn0929@gmail.com>
2021-11-06 02:25:20 +0900

    [feat] post delete button

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -29,3 +29,4 @@ Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middle
 Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
 Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
 Route::patch('admin/posts/{post}/', [AdminPostController::class, 'update'])->middleware('admin');
+Route::delete('admin/posts/{post}/', [AdminPostController::class, 'destroy'])->middleware('admin');
