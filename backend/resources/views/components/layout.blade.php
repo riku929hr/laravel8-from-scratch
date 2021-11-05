3ae023d8fe26aa9a744bd83ed2673114de7887e9
riku929hr <sugarhorn0929@gmail.com>
2021-11-06 01:25:28 +0900

    [feat] add posts list to the dashboard

--- backend/resources/views/components/layout.blade.php
+++ backend/resources/views/components/layout.blade.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 
 <title>Laravel From Scratch Blog</title>
@@ -8,10 +7,9 @@
 <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 <style>
-    html{
+    html {
         scroll-behavior: smooth;
     }
-
 </style>
 
 
@@ -32,9 +30,12 @@
                         <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!!</button>
                     </x-slot>
 
-                    <x-dropdown-item href="/admin/dashboard" >Dashboard</x-dropdown-item>
-                    <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
-                    <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
+                    <x-dropdown-item href="/admin/posts" :active="request()->is('admin/posts')">Dashboard
+                    </x-dropdown-item>
+                    <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post
+                    </x-dropdown-item>
+                    <x-dropdown-item href="#" x-data="{}"
+                        @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
 
                     <form id="logout-form" method="POST" action="/logout" class="hidden">
                         @csrf
@@ -47,7 +48,8 @@
 
                 @endauth
 
-                <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+                <a href="#newsletter"
+                    class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
                 </a>
             </div>
@@ -55,7 +57,8 @@
 
         {{ $slot }}
 
-        <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+        <footer id="newsletter"
+            class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
             <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
             <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
@@ -72,23 +75,18 @@
                             </label>
 
                             <div>
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="text"
-                                    placeholder="Your email address"
+                                <input id="email" name="email" type="text" placeholder="Your email address"
                                     class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
 
-                                    @error('email')
-                                        <span class="text-xs text-red-500">{{ $message }}</span>
-                                    @enderror
+                                @error('email')
+                                <span class="text-xs text-red-500">{{ $message }}</span>
+                                @enderror
                             </div>
 
                         </div>
 
                         <button type="submit"
-                                class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
-                        >
+                            class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8">
                             Subscribe
                         </button>
                     </form>
@@ -97,6 +95,6 @@
         </footer>
     </section>
 
-    <x-flash/>
+    <x-flash />
 
 </body>
